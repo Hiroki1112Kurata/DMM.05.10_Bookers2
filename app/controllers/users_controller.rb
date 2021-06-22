b841e0efabe2c93cbdd3a8b749825c705bdac704
Hiroki1112Kurata <sequoia1112@yahoo.co.jp>
2021-06-22 06:30:59 +0000

    [Add]5-10_uses_show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,14 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @books = @user.books.all
+  end
+
+  def index
+    @user = User.find(params[:id])
+  end
+
+  def edit
+
   end
 end
