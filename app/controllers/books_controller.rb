e49ffb36304f95f6a5de01774c805f1cd9f5c243
Hiroki1112Kurata <sequoia1112@yahoo.co.jp>
2021-06-22 14:06:25 +0000

    [Add]5-10_部分テンプレート

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,18 +8,22 @@ class BooksController < ApplicationController
     @book =Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to books_path
-    # ★リダイレクト先要注意
+    redirect_to book_path(@book.id)
+    # ★book showページへ移動
   end
 
   def index
     @books = Book.all
+    @bookn = Book.new
+    @user = User.find_by id: current_user.id
   end
 
   def show
+    @bookn = Book.new
     @book = Book.find(params[:id])
+    @user = User.find_by id: @book.user_id
   end
-  
+
   def edit
     @book = Book.find(params[:id])
   end
@@ -34,6 +38,9 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :body)
-
   end
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,22 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @books = @user.books.all
+    @book = Book.new
   end
 
   def index
     @users = User.all
+    @bookn = Book.new
+    @user = User.find_by id: current_user.id
   end
 
   def edit
 
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
   end
 end
