1f4ffebac0ea554ec87c415ff55dd46817d0d906
Hiroki1112Kurata <sequoia1112@yahoo.co.jp>
2021-06-23 12:48:45 +0000

    [Add]5-10_validate_user/edit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-    has_many :books, dependent: :destroy
-    
-  attachment :profile_image 
+  has_many :books, dependent: :destroy
+
+  validates :name, uniqueness: true, length: { minimum: 2, maximum: 20 }
+
+  attachment :profile_image
 
 end
